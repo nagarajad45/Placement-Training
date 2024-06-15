d93cce0e65fde1eaa3b7950ec7752f4a6d24d00a
Nagaraja Devaramani <160519007+nagarajad45@users.noreply.github.com>
2024-06-15 09:11:25 +0530

    Add files via upload

---
+++ Problems/Haker_Rank/linkedList.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *next;  
+};
+
+struct node *head;
+
+void start_insert();
+void last_insert();
+void random_insert();
+void start_delete();
+void last_delete();
+void random_delete();
+void search();
+void display();
+
+void main()
+{
+
+}
+
+void start_insert()
+{
+    struct node *ptr;
+    int item;
+    ptr = (struct node*)
+}
+
+
+
